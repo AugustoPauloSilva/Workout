f8094c08e29bbe0a340f86833b04fbd2c0fa11ff
Augusto Paulo <augustopaulo123@hotmail.com>
2021-09-09 14:47:04 -0300

    Implemented click actions for WorkoutListFragment

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -7,21 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WorkoutDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WorkoutDetailFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private long workoutId;
 
@@ -29,20 +15,9 @@ public class WorkoutDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WorkoutDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static WorkoutDetailFragment newInstance(String param1, String param2) {
         WorkoutDetailFragment fragment = new WorkoutDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,10 +29,6 @@ public class WorkoutDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
